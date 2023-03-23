84d1f0e443642dc381294b52956161177c5f8022
Richard Pires <richardjpires@gmail.com>
2023-03-23 10:53:20 -0300

    Added page loader and error message. Refactored styles.

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,9 @@ export default function Home() {
   const [ageInput, setAgeInput] = useState("modern");
   const [result, setResult] = useState();
   const [isLoading, setIsLoading] = useState(false);
+  const [pageReady, setPageReady] = useState(false);
   const [heroImage, setHeroImage] = useState();
+  const [showError, setShowError] = useState(false);
 
   const override = {
     display: "block",
@@ -30,9 +32,10 @@ export default function Home() {
 
         const data = await response.json();
         setHeroImage(data.image);
+        setPageReady(true);
       } catch (error) {
-        console.error(error);
         setIsLoading(false);
+        setPageReady(true);
       }
     }
 
@@ -43,6 +46,7 @@ export default function Home() {
     event.preventDefault();
     setIsLoading(true);
     setResult("");
+    setShowError(false);
 
     // submit openai prompt
     try {
@@ -67,6 +71,7 @@ export default function Home() {
     } catch (error) {
       console.error(error);
       setIsLoading(false);
+      setShowError(true);
     }
   }
 
@@ -78,54 +83,70 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <img src="/logo.png" className={styles.icon} alt="play what logo" />
-        <h1>playwhat</h1>
-
-        <div className={styles.formContainer}>
-          {heroImage && <img src={heroImage} alt="playwhat" />}
-
-          <form onSubmit={onSubmit}>
-            <span className={styles.inlineForm}>
-              {/* age */}
-              <div>
-                <span className={styles.text}>I wanna play a</span>{" "}
-                <select
-                  name="age"
-                  className={styles.age}
-                  value={ageInput}
-                  onChange={(e) => setAgeInput(e.target.value)}
-                >
-                  <option value="classic">classic</option>
-                  <option value="modern">modern</option>
-                </select>{" "}
-              </div>
-              {/* name */}
-              <div>
-                <span className={styles.text}>game like</span>{" "}
-                <input
-                  className={`${styles.inline} ${styles.game}`}
-                  type="text"
-                  name="game"
-                  placeholder="game"
-                  value={gameInput}
-                  onChange={(e) => setGameInput(e.target.value)}
-                />
-              </div>
-            </span>
-            {/* submit */}
-            <div className={styles.submitContainer}>
-              {isLoading ? (
-                <>
-                  <input type="submit" className={`${styles.submit} ${styles.loading}`} value=""></input>
-
-                  <PacmanLoader cssOverride={override} className={styles.loader} color="#484848" />
-                </>
-              ) : (
-                <input type="submit" className={styles.submit} value="recommend me" />
-              )}
-            </div>
-          </form>
+        <div className={`${styles.header} ${pageReady ? "" : styles.isLoading}`}>
+          <img src="/logo.png" className={styles.icon} alt="play what logo" />
+          <h1>playwhat</h1>
         </div>
+
+        {pageReady && (
+          <div className={styles.formContainer}>
+            {/* hero img */}
+            {heroImage && <img src={heroImage} alt="playwhat" />}
+
+            {/* form */}
+            <form onSubmit={onSubmit}>
+              <span className={styles.inlineForm}>
+                {/* age */}
+                <div>
+                  <span className={styles.text}>I wanna play a</span>{" "}
+                  <select
+                    name="age"
+                    className={styles.age}
+                    value={ageInput}
+                    onChange={(e) => setAgeInput(e.target.value)}
+                  >
+                    <option value="classic">classic</option>
+                    <option value="modern">modern</option>
+                  </select>{" "}
+                </div>
+                {/* name */}
+                <div>
+                  <span className={styles.text}>game like</span>{" "}
+                  <input
+                    className={`${styles.inline} ${styles.game}`}
+                    type="text"
+                    name="game"
+                    placeholder="game"
+                    value={gameInput}
+                    onChange={(e) => setGameInput(e.target.value)}
+                  />
+                </div>
+              </span>
+
+              {/* submit */}
+              <div className={styles.submitContainer}>
+                {isLoading ? (
+                  <>
+                    <input type="submit" className={`${styles.submit} ${styles.loading}`} value=""></input>
+
+                    <PacmanLoader cssOverride={override} className={styles.loader} color="#484848" />
+                  </>
+                ) : (
+                  <input type="submit" className={styles.submit} value="recommend me" />
+                )}
+              </div>
+
+              {/* error */}
+              {showError && (
+                <div className={styles.error}>
+                  Something went wrong :(
+                  <br />
+                  Please try again
+                </div>
+              )}
+            </form>
+          </div>
+        )}
 
         <div className={styles.result}>{formatResponse(result, heroImage)}</div>
       </main>
