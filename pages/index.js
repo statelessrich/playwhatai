c32a929bdeb23593496a50c60ab13dceca135caf
Richard Pires <richardjpires@gmail.com>
2023-03-23 22:01:38 -0300

    Improved hero image size. Added meta tags.

--- pages/index.js
+++ pages/index.js
@@ -106,6 +106,11 @@ export default function Home() {
     <div>
       <Head>
         <title>playwhat</title>
+        <meta property="og:title" content="playwhat" key="title" />
+        <meta property="og:description" content="Video game recommendations using AI." />
+        <meta property="og:keywords" content="video games, AI, gaming" />
+        <meta property="og:image" content="https://playwhatai.vercel.app/social.png" />
+        <meta property="og:url" content="https://playwhatai.vercel.app/" />
         <link rel="icon" href="/logo.png" />
       </Head>
 
