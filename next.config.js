0c661fee96bb576b421d29514099cacc4049f0d4
Richard <richardjpires@gmail.com>
2023-06-27 20:09:50 -0600

    4th attempt

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["media.rawg.io"],
-  },
-  webpack: (config, options) => {
-    if (config.resolve.plugins) {
-      config.resolve.plugins.push(new TsconfigPathsPlugin());
-    } else {
-      config.resolve.plugins = [new TsconfigPathsPlugin()];
-    }
-
-    return config;
   },
 };
 
