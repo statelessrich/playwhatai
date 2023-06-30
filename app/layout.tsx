5993f1d840f9ae6b002e28d0950db12bf88344c0
Richard <richardjpires@gmail.com>
2023-06-29 18:21:03 -0600

    6th attempt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.scss";
-import Header from "components/Header";
-import Footer from "components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export const metadata = {
   title: "playwhat",
