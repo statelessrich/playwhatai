eb9f68545de03c1d02ba534ee81afd623d9e8cad
richard <richardjpires@gmail.com>
2025-04-24 19:35:00 -0500

    fix redux

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,14 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import homeReducer from "./features/homeSlice";
 import formReducer from "./features/formSlice";
-import { AnyAction } from "redux";
+import homeReducer from "./features/homeSlice";
 
 export const store = configureStore({
-  reducer: {
-    home: homeReducer,
-    form: formReducer,
-    devTools: (state: boolean = process.env.NODE_ENV !== "production", action: AnyAction) => state,
-  },
+	reducer: {
+		home: homeReducer,
+		form: formReducer,
+	},
+	devTools: process.env.NODE_ENV !== "production",
 });
 
 export type RootState = ReturnType<typeof store.getState>;
